4bd763a00c6759cc93845b0d30c439f96c3a8edf
Mike Kazantsev <mk.fraggod@gmail.com>
2012-09-17 11:01:21 +0600

    Working authentication methods

--- skydrive/api_v5.py
+++ skydrive/api_v5.py
@@ -1,9 +1,10 @@
 #-*- coding: utf-8 -*-
-from __future__ import print_function
+from __future__ import unicode_literals, print_function
 
 
 import itertools as it, operator as op, functools as ft
-import os, sys, math, urllib, urlparse
+from datetime import datetime, timedelta
+import os, sys, math, urllib, urlparse, json
 import requests
 
 
@@ -16,8 +17,8 @@ class AuthenticationError(SkyDriveInteractionError): pass
 
 def request_post(url, data):
 	try:
-		return requests.post( url,
-			data=data, config=dict(danger_mode=True) ).json
+		return json.loads(requests.post( url,
+			data=data, config=dict(danger_mode=True) ).text)
 	except requests.RequestException as err:
 		raise ProtocolError(err.message)
 
@@ -28,6 +29,14 @@ def urandom_hex(n):
 
 class SkyDriveAPIv5(object):
 
+	auth_url_user = 'https://login.live.com/oauth20_authorize.srf'
+	auth_url_token = 'https://login.live.com/oauth20_token.srf'
+	auth_scope = 'wl.skydrive', 'wl.skydrive_update', 'wl.offline_access'
+	auth_redirect_uri_mobile = 'https://login.live.com/oauth20_desktop.srf'
+
+	# Set by auth_get_token, not used internally
+	auth_expires = auth_access_data_raw = None
+
 	# Should be static on per-application basis.
 	# Can be received from LiveConnect by any registered user at https://manage.dev.live.com/
 	# API ToS can be found here at http://msdn.microsoft.com/en-US/library/live/ff765012
@@ -37,11 +46,7 @@ class SkyDriveAPIv5(object):
 	auth_code = auth_refresh_token = auth_access_token = None
 
 	# This (default) redirect_uri is **special** - app must be marked as "mobile" to use it.
-	auth_redirect_uri = 'https://login.live.com/oauth20_desktop.srf'
-
-	auth_url_user = 'https://login.live.com/oauth20_authorize.srf'
-	auth_url_token = 'https://login.live.com/oauth20_token.srf'
-	auth_scope = 'wl.skydrive', 'wl.skydrive_update', 'wl.offline_access'
+	auth_redirect_uri = auth_redirect_uri_mobile
 
 
 	def __init__(self, client_id=None, client_secret=None, urandom_len=16, **config):
@@ -60,11 +65,11 @@ class SkyDriveAPIv5(object):
 		return cls(**yaml.load(open(os.path.expanduser('~/.lcrc')).read()))
 
 
-	def auth_user_get_url(self, scope=None, redirect_uri=None):
+	def auth_user_get_url(self, scope=None):
 		# Note: default redirect_uri is **special**, app must be marked as "mobile" to use it
 		return '{}?{}'.format( self.auth_url_user, urllib.urlencode(dict(
 			client_id=self.client_id, scope=' '.join(scope or self.auth_scope),
-			response_type='code', redirect_uri=redirect_uri or self.auth_redirect_uri )) )
+			response_type='code', redirect_uri=self.auth_redirect_uri )) )
 
 	def auth_user_process_url(self, url):
 		url = urlparse.urlparse(url)
@@ -76,24 +81,47 @@ class SkyDriveAPIv5(object):
 		self.auth_code = url_qs['code']
 		return self.auth_code
 
-	def auth_get_token(self, refresh_token=None):
-		if refresh_token is None:
-			if not self.auth_refresh_token and not self.auth_code:
-				raise AuthenticationError( 'Either code or refresh_token'
-					' is necessary to request access_token, but neither one was provided.' )
-			refresh_token = self.auth_refresh_token
-		res = request_post(
-			data=dict(code=self.auth_code, grant_type='authorization_code')
-				if not refresh_token else
-				dict(refresh_token=refresh_token, grant_type='refresh_token') )
+
+	def auth_get_token( self, check_scope=True,
+			_check_keys=['client_id', 'client_secret', 'code', 'refresh_token', 'grant_type'] ):
+
+		post_data = dict( client_id=self.client_id,
+			client_secret=self.client_secret, redirect_uri=self.auth_redirect_uri )
+		if not self.auth_refresh_token:
+			post_data.update(code=self.auth_code, grant_type='authorization_code')
+		else:
+			if self.auth_redirect_uri == self.auth_redirect_uri_mobile:
+				del post_data['client_secret'] # not necessary for "mobile" apps
+			post_data.update(
+				refresh_token=self.auth_refresh_token, grant_type='refresh_token' )
+		post_data_missing_keys = list( k for k in
+			_check_keys if k in post_data and not post_data[k] )
+		if post_data_missing_keys:
+			raise AuthenticationError( 'Insufficient authentication'
+				' data provided (missing keys: {})'.format(post_data_missing_keys) )
+
+		res = self.auth_access_data_raw =\
+			request_post(self.auth_url_token, data=post_data)
+
+		assert res['token_type'] == 'bearer'
 		for k in 'access_token', 'refresh_token':
 			if k in res: setattr(self, 'auth_{}'.format(k), res[k])
-		print(res)
+		self.auth_expires = None if 'expires_in' not in res\
+			else (datetime.now() + timedelta(0, res['expires_in']))
+
+		scope_granted = res.get('scope', '').split()
+		if check_scope and set(self.auth_scope) != set(scope_granted):
+			raise AuthenticationError(
+				"Granted scope ({}) doesn't match requested one ({})."\
+				.format(', '.join(scope_granted), ', '.join(self.auth_scope)) )
+		return scope_granted
+
 
 
 if __name__ == '__main__':
 	api = SkyDriveAPIv5.from_lcrc()
-	# print(api.auth_user_get_url())
-	api.auth_user_process_url( 'https://login.live.com/'
-		'oauth20_desktop.srf?code=d47e2f08-0851-cf5c-86c7-8fdcd9db128c&lc=1033' )
-	# api.auth_get_token(
+	print(api.auth_user_get_url())
+	# api.auth_user_process_url( 'https://login.live.com/'
+	# 	'oauth20_desktop.srf?code=d47e2f08-0851-cf5c-86c7-8fdcd9db128c&lc=1033' )
+	# print(api.auth_get_token())
+	# print(api.auth_access_data_raw)
