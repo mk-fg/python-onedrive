04d20cb617a0c8a3d16217184f5e61392a8980e6
AntonioChen <chenxuanyi1991@gmail.com>
2013-02-19 00:24:02 +0800

    [win7] auth completed.

--- skydrive/portalocker.py
+++ skydrive/portalocker.py
@@ -1,5 +1,5 @@
 #-*- coding: utf-8 -*-
-__author__ = 'Antonio'
+
 import os
 # needs win32all to work on Windows (NT, 2K, XP, _not_ /95 or /98)
 if os.name == 'nt':
@@ -12,11 +12,11 @@ if os.name == 'nt':
 
     def lock(file, flags):
         hfile = win32file._get_osfhandle(file.fileno())
-        win32file.LockFileEx(hfile, flags, 0, 0xffff0000, __overlapped)
+        win32file.LockFileEx(hfile, flags, 0, 0x7FFFFFFF, __overlapped)
 
     def unlock(file):
         hfile = win32file._get_osfhandle(file.fileno())
-        win32file.UnlockFileEx(hfile, 0, 0xffff0000, __overlapped)
+        win32file.UnlockFileEx(hfile, 0, 0x7FFFFFFF, __overlapped)
 elif os.name == 'posix':
     from fcntl import LOCK_EX, LOCK_SH, LOCK_NB
 
