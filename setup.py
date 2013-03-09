mk-fg/python-onedrive

commit a2bbccdcc67b7f2208c0193dc7c96bcaa534b8a9

Misc code style tweaks (spaces, pep8)

setup.py
@@ -6,50 +6,52 @@ import os
 pkg_root = os.path.dirname(__file__)
 
 # Error-handling here is to allow package to be built w/o README included
-try: readme = open(os.path.join(pkg_root, 'README.txt')).read()
-except IOError: readme = ''
+try:
+    readme = open(os.path.join(pkg_root, 'README.txt')).read()
+except IOError:
+    readme = ''
 
 setup(
 
-	name = 'python-skydrive',
-	version = '13.01.2',
-	author = 'Mike Kazantsev',
-	author_email = 'mk.fraggod@gmail.com',
-	license = 'WTFPL',
-	keywords = 'skydrive api oauth2 rest microsoft cloud live liveconnect',
-	url = 'http://github.com/mk-fg/python-skydrive',
+    name='python-skydrive',
+    version='13.03.3',
+    author='Mike Kazantsev',
+    author_email='mk.fraggod@gmail.com',
+    license='WTFPL',
+    keywords='skydrive api oauth2 rest microsoft cloud live liveconnect',
+    url='http://github.com/mk-fg/python-skydrive',
 
-	description = 'Python and command-line interface'
-		' for Microsoft LiveConnect SkyDrive REST API v5.0',
-	long_description = readme,
+    description='Python and command-line interface'
+                ' for Microsoft LiveConnect SkyDrive REST API v5.0',
+    long_description=readme,
 
-	classifiers = [
-		'Development Status :: 4 - Beta',
-		'Environment :: Console',
-		'Intended Audience :: Developers',
-		'Intended Audience :: System Administrators',
-		'Intended Audience :: Information Technology',
-		'License :: OSI Approved',
-		'Operating System :: OS Independent',
-		'Programming Language :: Python',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 2 :: Only',
-		'Topic :: Internet',
-		'Topic :: Software Development',
-		'Topic :: System :: Archiving',
-		'Topic :: System :: Filesystems',
-		'Topic :: Utilities' ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 2 :: Only',
+        'Topic :: Internet',
+        'Topic :: Software Development',
+        'Topic :: System :: Archiving',
+        'Topic :: System :: Filesystems',
+        'Topic :: Utilities'],
 
-	# install_requires = [],
-	extras_require = dict(
-		standalone=['requests'],
-		cli=['PyYAML', 'requests'],
-		conf=['PyYAML', 'requests'] ),
+    # install_requires = [],
+    extras_require=dict(
+        standalone=['requests'],
+        cli=['PyYAML', 'requests'],
+        conf=['PyYAML', 'requests']),
 
-	packages = find_packages(),
-	include_package_data = True,
-	package_data = {'': ['README.txt']},
-	exclude_package_data = {'': ['README.*']},
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['README.txt']},
+    exclude_package_data={'': ['README.*']},
 
-	entry_points = dict(console_scripts=[
-		'skydrive-cli = skydrive.cli_tool:main' ]) )
+    entry_points=dict(console_scripts=[
+        'skydrive-cli = skydrive.cli_tool:main']))

.gitignore
@@ -6,3 +6,4 @@
 /README.txt
 *.pyc
 *.pyo
+.idea

doc/conf.py
@@ -9,7 +9,6 @@ os.chdir(doc_root)
 sys.path.insert(0, abspath('..')) # for module itself
 sys.path.append(abspath('.')) # for extenstions
 
-
 needs_sphinx = '1.1'
 extensions = ['sphinx.ext.autodoc', 'sphinx_local_hooks']
 

doc/sphinx_local_hooks.py
@@ -5,71 +5,81 @@ import itertools as it, operator as op, functools as ft
 from collections import Iterable
 import os, sys, types, re
 
-
 from sphinx.ext.autodoc import Documenter
 
 _autodoc_add_line = Documenter.add_line
 
+
 @ft.wraps(_autodoc_add_line)
 def autodoc_add_line(self, line, *argz, **kwz):
-	tee = self.env.app.config.autodoc_dump_rst
-	if tee:
-		tee_line = self.indent + line
-		if isinstance(tee, file): tee.write(tee_line + '\n')
-		elif tee is True: print(tee_line)
-		else:
-			raise ValueError( 'Unrecognized'
-				' value for "autodoc_dump_rst" option: {!r}'.format(tee) )
-	return _autodoc_add_line(self, line, *argz, **kwz)
+    tee = self.env.app.config.autodoc_dump_rst
+    if tee:
+        tee_line = self.indent + line
+        if isinstance(tee, file):
+            tee.write(tee_line + '\n')
+        elif tee is True:
+            print(tee_line)
+        else:
+            raise ValueError('Unrecognized'
+                             ' value for "autodoc_dump_rst" option: {!r}'.format(tee))
+    return _autodoc_add_line(self, line, *argz, **kwz)
+
 
 Documenter.add_line = autodoc_add_line
 
 
 def process_docstring(app, what, name, obj, options, lines):
-	if not lines: return
+    if not lines: return
 
-	i, ld = 0, dict(enumerate(lines)) # to allow arbitrary peeks
-	i_max = max(ld)
+    i, ld = 0, dict(enumerate(lines)) # to allow arbitrary peeks
+    i_max = max(ld)
 
-	def process_line(i):
-		line, i_next = ld[i], i + 1
-		while i_next not in ld and i_next <= i_max: i_next += 1
-		line_next = ld.get(i_next)
+    def process_line(i):
+        line, i_next = ld[i], i + 1
+        while i_next not in ld and i_next <= i_max: i_next += 1
+        line_next = ld.get(i_next)
 
-		if line_next and line_next[0] in u' \t': # tabbed continuation of the sentence
-			ld[i] = u'{} {}'.format(line, line_next.strip())
-			del ld[i_next]
-			process_line(i)
-		elif line.endswith(u'.') or (line_next and line_next[0].isupper()): ld[i+0.5] = u''
+        if line_next and line_next[0] in u' \t': # tabbed continuation of the sentence
+            ld[i] = u'{} {}'.format(line, line_next.strip())
+            del ld[i_next]
+            process_line(i)
+        elif line.endswith(u'.') or (line_next and line_next[0].isupper()):
+            ld[i + 0.5] = u''
 
-	for i in xrange(i_max + 1):
-		if i not in ld: continue # was removed
-		process_line(i)
+    for i in xrange(i_max + 1):
+        if i not in ld: continue # was removed
+        process_line(i)
 
-	# Overwrite the list items inplace, extending the list if necessary
-	for i, (k, line) in enumerate(sorted(ld.viewitems())):
-		try: lines[i] = line
-		except IndexError: lines.append(line)
+    # Overwrite the list items inplace, extending the list if necessary
+    for i, (k, line) in enumerate(sorted(ld.viewitems())):
+        try:
+            lines[i] = line
+        except IndexError:
+            lines.append(line)
 
 
 def skip_override(app, what, name, obj, skip, options):
-	if options.get('exclude-members'):
-		include_only = set( re.compile(k[3:])
-			for k in options['exclude-members'] if k.startswith('rx:') )
-		if include_only:
-			for pat in include_only:
-				if pat.search(name): break
-			else: return True
-	if what == 'exception':
-		return False if name == '__init__'\
-			and isinstance(obj, types.UnboundMethodType) else True
-	elif what == 'class':
-		if name in ['__init__', '__call__']\
-			and isinstance(obj, types.UnboundMethodType): return False
-		elif getattr(obj, 'im_class', None) is type: return False
-	return skip
+    if options.get('exclude-members'):
+        include_only = set(re.compile(k[3:])
+            for k in options['exclude-members'] if k.startswith('rx:'))
+        if include_only:
+            for pat in include_only:
+                if pat.search(name): break
+            else:
+                return True
+    if what == 'exception':
+        return False if name == '__init__' \
+            and isinstance(obj, types.UnboundMethodType) else True
+    elif what == 'class':
+        if name in ['__init__', '__call__'] \
+            and isinstance(obj, types.UnboundMethodType):
+            return False
+        elif getattr(obj, 'im_class', None) is type:
+            return False
+    return skip
+
 
 def setup(app):
-	app.connect('autodoc-process-docstring', process_docstring)
-	app.connect('autodoc-skip-member', skip_override)
-	app.add_config_value('autodoc_dump_rst', None, True)
+    app.connect('autodoc-process-docstring', process_docstring)
+    app.connect('autodoc-skip-member', skip_override)
+    app.add_config_value('autodoc_dump_rst', None, True)

doc/sphinx_text_to_md.py
@@ -10,71 +10,74 @@ class FormatError(Exception): pass
 
 
 def main():
-	import argparse
-	parser = argparse.ArgumentParser(
-		description='Convert sphinx-produced autodoc.apidoc text to markdown.')
-	parser.add_argument('src', nargs='?', help='Source file (default: use stdin).')
-	optz = parser.parse_args()
+    import argparse
 
-	src = open(optz.src) if optz.src else sys.stdin
-	dst = sys.stdout
+    parser = argparse.ArgumentParser(
+        description='Convert sphinx-produced autodoc.apidoc text to markdown.')
+    parser.add_argument('src', nargs='?', help='Source file (default: use stdin).')
+    optz = parser.parse_args()
 
-	py_name = r'[\w_\d]+'
-	out = ft.partial(print, file=dst)
+    src = open(optz.src) if optz.src else sys.stdin
+    dst = sys.stdout
 
-	st_attrdoc = 0
-	st_cont, lse_nl = False, None
+    py_name = r'[\w_\d]+'
+    out = ft.partial(print, file=dst)
 
-	for line in src:
-		ls = line.strip()
-		if not ls: # blank line
-			out(line, end='')
-			continue
+    st_attrdoc = 0
+    st_cont, lse_nl = False, None
 
-		line_indent = re.search(r'^( +)', line)
-		if not line_indent: line_indent = 0
-		else:
-			line_indent = len(line_indent.group(1))
-			if line_indent % 3: raise FormatError('Weird indent size: {}'.format(line_indent))
-			line_indent = line_indent / 3
+    for line in src:
+        ls = line.strip()
+        if not ls: # blank line
+            out(line, end='')
+            continue
 
-		lp = line.split()
-		lse = re.sub(r'(<\S+) at 0x[\da-f]+(>)', r'\1\2', ls)
-		lse = re.sub(r'([_*<>])', r'\\\1', lse)
-		for url in re.findall(r'\b\w+://\S+', lse):
-			lse = lse.replace(url, url.replace(r'\_', '_'))
-		lse = re.sub(r'\bu([\'"])', r'\1', lse)
-		st_cont, lse_nl = bool(lse_nl), '' if re.search(r'\b\w+://\S+-$', lse) else '\n'
+        line_indent = re.search(r'^( +)', line)
+        if not line_indent:
+            line_indent = 0
+        else:
+            line_indent = len(line_indent.group(1))
+            if line_indent % 3: raise FormatError('Weird indent size: {}'.format(line_indent))
+            line_indent = line_indent / 3
 
-		st_attrdoc_reset = True
-		if not line_indent:
-			if len(lp) > 2 and lp[0] == lp[1]:
-				if lp[0] in ('exception', 'class'): # class, exception
-					out('\n'*1, end='')
-					out('* **{}**'.format(' '.join(lse.split()[1:])))
+        lp = line.split()
+        lse = re.sub(r'(<\S+) at 0x[\da-f]+(>)', r'\1\2', ls)
+        lse = re.sub(r'([_*<>])', r'\\\1', lse)
+        for url in re.findall(r'\b\w+://\S+', lse):
+            lse = lse.replace(url, url.replace(r'\_', '_'))
+        lse = re.sub(r'\bu([\'"])', r'\1', lse)
+        st_cont, lse_nl = bool(lse_nl), '' if re.search(r'\b\w+://\S+-$', lse) else '\n'
 
-			else:
-				raise FormatError('Unhandled: {!r}'.format(line))
+        st_attrdoc_reset = True
+        if not line_indent:
+            if len(lp) > 2 and lp[0] == lp[1]:
+                if lp[0] in ('exception', 'class'): # class, exception
+                    out('\n' * 1, end='')
+                    out('* **{}**'.format(' '.join(lse.split()[1:])))
 
-		elif line_indent == 1:
-			if re.search(r'^(\w+ )?{}\('.format(py_name), ls): # function
-				out('\n'*1, end='')
-				out('{}* {}'.format(' '*4, lse))
-				st_attrdoc, st_attrdoc_reset = 8, False
-			elif re.search(r'^{}\s+=\s+'.format(py_name), ls): # attribute
-				out('{}* {}'.format(' '*4, lse))
-				st_attrdoc, st_attrdoc_reset = 8, False
-			elif lp[0] == 'Bases:': # class bases
-				out('{}{}'.format(' '*4, lse))
-				st_attrdoc, st_attrdoc_reset = 4, False
-			else: out('{}{}'.format(' '*(4 * st_cont), ls), end=lse_nl) # class docstring
+            else:
+                raise FormatError('Unhandled: {!r}'.format(line))
 
-		else: # description line
-			if ls[0] in '-*': line = '\\' + line.lstrip()
-			out('{}{}'.format(' '*(st_attrdoc * st_cont), line.strip()), end=lse_nl)
-			st_attrdoc_reset = False
+        elif line_indent == 1:
+            if re.search(r'^(\w+ )?{}\('.format(py_name), ls): # function
+                out('\n' * 1, end='')
+                out('{}* {}'.format(' ' * 4, lse))
+                st_attrdoc, st_attrdoc_reset = 8, False
+            elif re.search(r'^{}\s+=\s+'.format(py_name), ls): # attribute
+                out('{}* {}'.format(' ' * 4, lse))
+                st_attrdoc, st_attrdoc_reset = 8, False
+            elif lp[0] == 'Bases:': # class bases
+                out('{}{}'.format(' ' * 4, lse))
+                st_attrdoc, st_attrdoc_reset = 4, False
+            else:
+                out('{}{}'.format(' ' * (4 * st_cont), ls), end=lse_nl) # class docstring
 
-		if st_attrdoc and st_attrdoc_reset: st_attrdoc = 0
+        else: # description line
+            if ls[0] in '-*': line = '\\' + line.lstrip()
+            out('{}{}'.format(' ' * (st_attrdoc * st_cont), line.strip()), end=lse_nl)
+            st_attrdoc_reset = False
+
+        if st_attrdoc and st_attrdoc_reset: st_attrdoc = 0
 
 
 if __name__ == '__main__': main()

skydrive/conf.py
@@ -1,124 +1,127 @@
 #-*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
 
-
 import itertools as it, operator as op, functools as ft
 import os, sys, io, errno, tempfile, fcntl, stat
 from os.path import dirname, basename
 
 import logging
+
 log = logging.getLogger(__name__)
 
 
 class ConfigMixin(object):
+    #: Path to configuration file to use in from_conf() by default.
+    conf_path_default = b'~/.lcrc'
 
-	#: Path to configuration file to use in from_conf() by default.
-	conf_path_default = b'~/.lcrc'
+    #: If set to some path, updates will be written back to it.
+    conf_save = False
 
-	#: If set to some path, updates will be written back to it.
-	conf_save = False
+    #: Raise human-readable errors on structure issues,
+    #:  which assume that there is an user-accessible configuration file
+    conf_raise_structure_errors = False
 
-	#: Raise human-readable errors on structure issues,
-	#:  which assume that there is an user-accessible configuration file
-	conf_raise_structure_errors = False
-
-	#: Hierarchical list of keys to write back
-	#:  to configuration file (preserving the rest) on updates.
-	conf_update_keys = dict(
-		client={'id', 'secret'},
-		auth={'code', 'refresh_token', 'access_expires', 'access_token'} )
-
-
-	def __init__(self, **kwz):
-		raise NotImplementedError('Init should be overidden with something configurable')
+    #: Hierarchical list of keys to write back
+    #:  to configuration file (preserving the rest) on updates.
+    conf_update_keys = dict(
+        client={'id', 'secret'},
+        auth={'code', 'refresh_token', 'access_expires', 'access_token'})
 
 
-	@classmethod
-	def from_conf(cls, path=None, **overrides):
-		'''Initialize instance from YAML configuration file,
-			writing updates (only to keys, specified by "conf_update_keys") back to it.'''
-		import yaml
-
-		if path is None:
-			path = cls.conf_path_default
-			log.debug('Using default state-file path: {}'.format(path))
-		path = os.path.expanduser(path)
-		with open(path) as src:
-			fcntl.lockf(src, fcntl.LOCK_SH)
-			conf = yaml.load(src.read())
-		conf.setdefault('conf_save', path)
-
-		conf_cls = dict()
-		for ns, keys in cls.conf_update_keys.viewitems():
-			for k in keys:
-				try: v = conf.get(ns, dict()).get(k)
-				except AttributeError:
-					if not cls.conf_raise_structure_errors: raise
-					raise KeyError( 'Unable to get value for configuration parameter'
-						' "{k}" in section "{ns}", check configuration file (path: {path}) syntax'
-						' near the aforementioned section/value.'.format(ns=ns, k=k, path=path) )
-				if v is not None:
-					conf_cls['{}_{}'.format(ns, k)] = conf[ns][k]
-		conf_cls.update(overrides)
-
-		self = cls(**conf_cls)
-		self.conf_save = conf['conf_save']
-		return self
+    def __init__(self, **kwz):
+        raise NotImplementedError('Init should be overidden with something configurable')
 
 
-	def sync(self):
-		if not self.conf_save: return
-		import yaml
+    @classmethod
+    def from_conf(cls, path=None, **overrides):
+        '''Initialize instance from YAML configuration file,
+            writing updates (only to keys, specified by "conf_update_keys") back to it.'''
+        import yaml
 
-		retry = False
-		with open(self.conf_save, 'r+') as src:
-			fcntl.lockf(src, fcntl.LOCK_SH)
-			conf_raw = src.read()
-			conf = yaml.load(io.BytesIO(conf_raw)) if conf_raw else dict()
+        if path is None:
+            path = cls.conf_path_default
+            log.debug('Using default state-file path: {}'.format(path))
+        path = os.path.expanduser(path)
+        with open(path) as src:
+            fcntl.lockf(src, fcntl.LOCK_SH)
+            conf = yaml.load(src.read())
+        conf.setdefault('conf_save', path)
 
-			conf_updated = False
-			for ns, keys in self.conf_update_keys.viewitems():
-				for k in keys:
-					v = getattr(self, '{}_{}'.format(ns, k), None)
-					if isinstance(v, unicode): v = v.encode('utf-8')
-					if v != conf.get(ns, dict()).get(k):
-						# log.debug(
-						# 	'Different val ({}.{}): {!r} != {!r}'\
-						# 	.format(ns, k, v, conf.get(ns, dict()).get(k)) )
-						conf.setdefault(ns, dict())[k] = v
-						conf_updated = True
+        conf_cls = dict()
+        for ns, keys in cls.conf_update_keys.viewitems():
+            for k in keys:
+                try:
+                    v = conf.get(ns, dict()).get(k)
+                except AttributeError:
+                    if not cls.conf_raise_structure_errors: raise
+                    raise KeyError('Unable to get value for configuration parameter'
+                                   ' "{k}" in section "{ns}", check configuration file (path: {path}) syntax'
+                                   ' near the aforementioned section/value.'.format(ns=ns, k=k, path=path))
+                if v is not None:
+                    conf_cls['{}_{}'.format(ns, k)] = conf[ns][k]
+        conf_cls.update(overrides)
 
-			if conf_updated:
-				log.debug('Updating configuration file ({})'.format(src.name))
-				with tempfile.NamedTemporaryFile(
-						prefix='{}.'.format(basename(self.conf_save)),
-						dir=dirname(self.conf_save), delete=False ) as tmp:
-					try:
-						fcntl.lockf(tmp, fcntl.LOCK_EX)
-						yaml.safe_dump(conf, tmp, default_flow_style=False)
-						tmp.flush()
-						os.fchmod( tmp.fileno(),
-							stat.S_IMODE(os.fstat(src.fileno()).st_mode) )
+        self = cls(**conf_cls)
+        self.conf_save = conf['conf_save']
+        return self
 
-						fcntl.lockf(src, fcntl.LOCK_EX)
-						src.seek(0)
-						if src.read() != conf_raw: retry = True
-						else:
-							# Atomic update
-							os.rename(tmp.name, src.name)
 
-							# Non-atomic update for pids that already have fd to old file,
-							#  but (presumably) are waiting for the write-lock to be released
-							src.seek(0), tmp.seek(0)
-							src.truncate()
-							src.write(tmp.read())
-							src.flush()
+    def sync(self):
+        if not self.conf_save: return
+        import yaml
 
-					finally:
-						try: os.unlink(tmp.name)
-						except OSError: pass
+        retry = False
+        with open(self.conf_save, 'r+') as src:
+            fcntl.lockf(src, fcntl.LOCK_SH)
+            conf_raw = src.read()
+            conf = yaml.load(io.BytesIO(conf_raw)) if conf_raw else dict()
 
-		if retry:
-			log.debug(( 'Configuration file ({}) was changed'
-				' during merge, restarting merge' ).format(self.conf_save))
-			return self.sync()
+            conf_updated = False
+            for ns, keys in self.conf_update_keys.viewitems():
+                for k in keys:
+                    v = getattr(self, '{}_{}'.format(ns, k), None)
+                    if isinstance(v, unicode): v = v.encode('utf-8')
+                    if v != conf.get(ns, dict()).get(k):
+                        # log.debug(
+                        # 	'Different val ({}.{}): {!r} != {!r}'\
+                        # 	.format(ns, k, v, conf.get(ns, dict()).get(k)) )
+                        conf.setdefault(ns, dict())[k] = v
+                        conf_updated = True
+
+            if conf_updated:
+                log.debug('Updating configuration file ({})'.format(src.name))
+                with tempfile.NamedTemporaryFile(
+                        prefix='{}.'.format(basename(self.conf_save)),
+                        dir=dirname(self.conf_save), delete=False) as tmp:
+                    try:
+                        fcntl.lockf(tmp, fcntl.LOCK_EX)
+                        yaml.safe_dump(conf, tmp, default_flow_style=False)
+                        tmp.flush()
+                        os.fchmod(tmp.fileno(),
+                                  stat.S_IMODE(os.fstat(src.fileno()).st_mode))
+
+                        fcntl.lockf(src, fcntl.LOCK_EX)
+                        src.seek(0)
+                        if src.read() != conf_raw:
+                            retry = True
+                        else:
+                            # Atomic update
+                            os.rename(tmp.name, src.name)
+
+                            # Non-atomic update for pids that already have fd to old file,
+                            #  but (presumably) are waiting for the write-lock to be released
+                            src.seek(0), tmp.seek(0)
+                            src.truncate()
+                            src.write(tmp.read())
+                            src.flush()
+
+                    finally:
+                        try:
+                            os.unlink(tmp.name)
+                        except OSError:
+                            pass
+
+        if retry:
+            log.debug(( 'Configuration file ({}) was changed'
+                        ' during merge, restarting merge' ).format(self.conf_save))
+            return self.sync()

commit e708f0f075e6ce640ba8b281c4683880dff325c4

doc.api: fixed indents (were broken in a2bbccdcc)

setup.py
@@ -14,7 +14,7 @@ except IOError:
 setup(
 
     name='python-skydrive',
-    version='13.03.17',
+    version='13.03.18',
     author='Mike Kazantsev, Antonio Chen',
     author_email='mk.fraggod@gmail.com',
     license='WTFPL',