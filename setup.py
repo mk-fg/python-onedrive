0d6141d722f743c6ab93207b4fca88c31a8ae2d4
Mike Kazantsev <mk.fraggod@gmail.com>
2013-08-17 14:25:46 +0600

    setup: version bump

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except IOError:
 setup(
 
     name='python-skydrive',
-    version='13.06.4',
+    version='13.08.2',
     author='Mike Kazantsev, Antonio Chen',
     author_email='mk.fraggod@gmail.com',
     license='WTFPL',
