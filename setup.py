mk-fg/python-onedrive

commit 90114a9ad1085233140dd1bfb9a2f84674c1e6b3

api.request: return more descriptive error messages, derived from returned json, if possible

setup.py
@@ -14,7 +14,7 @@ except IOError:
 setup(
 
     name='python-skydrive',
-    version='13.11.0',
+    version='13.11.1',
     author='Mike Kazantsev, Antonio Chen',
     author_email='mk.fraggod@gmail.com',
     license='WTFPL',

skydrive/api_v5.py
@@ -156,7 +156,11 @@ class SkyDriveHTTPClient(object):
                 res.raise_for_status()
             return json.loads(res.text) if not raw else res.content
         except requests.RequestException as err:
-            raise raise_for.get(code, ProtocolError)(code, err.message)
+            try:
+                message = res.json()['error']
+            except (ValueError, KeyError):
+                message = err.message
+            raise raise_for.get(code, ProtocolError)(code, message)
 
 
 class SkyDriveAuth(SkyDriveHTTPClient):

commit f347810af219db8371ecc65851e47bfc456ad9f3

api.request: fix error reporting when request fails before getting any response
As pointed out in #10, introduced in 90114a9 (for #8).

setup.py
@@ -14,7 +14,7 @@ except IOError:
 setup(
 
     name='python-skydrive',
-    version='13.11.4',
+    version='13.12.0',
     author='Mike Kazantsev, Antonio Chen',
     author_email='mk.fraggod@gmail.com',
     license='WTFPL',

skydrive/api_v5.py
@@ -145,7 +145,7 @@ class SkyDriveHTTPClient(object):
             kwz['files'] = files
         if headers is not None:
             kwz['headers'] = headers
-        code = None
+        code = res = None
         try:
             res = func(url, **kwz)
             # log.debug('Response headers: {}'.format(res.headers))
@@ -157,6 +157,8 @@ class SkyDriveHTTPClient(object):
             return json.loads(res.text) if not raw else res.content
         except requests.RequestException as err:
             try:
+                if res is None:
+                    raise ValueError
                 message = res.json()['error']
             except (ValueError, KeyError):
                 message = err.message