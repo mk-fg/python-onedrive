mk-fg/python-onedrive

commit 9d5f00f70dfa7a059e9b3325cb55f4eca50b823c

api.put: use PUT requests, as suggested in API docs, fixes #19
Current docs clearly state:

  **Important**
  On platforms that support the PUT request, PUT should be used to
  upload files to OneDrive when possible to avoid errors that can
  occur when POST is used.

-- http://msdn.microsoft.com/en-us/library/dn659726.aspx

Probably wasn't there before, otherwise can't think why I used POST
in the first place.

onedrive/api_v5.py
@@ -140,7 +140,7 @@ class OneDriveHTTPClient(object):
                                     ft.partial(session.request, method.upper())
                                     )
         if data is not None:
-            if method == 'post':
+            if method in ['post', 'put']:
                 kwz['data'] = data
             else:
                 kwz['data'] = json.dumps(data)
@@ -387,8 +387,9 @@ class OneDriveAPIWrapper(OneDriveAuth):
             if isinstance(path_or_tuple, types.StringTypes) \
             else (path_or_tuple[0], path_or_tuple[1])
 
-        return self(ujoin(folder_id, 'files'), dict(overwrite=overwrite),
-                    method='post', files=dict(file=(name, src)))
+        return self(ujoin(folder_id, 'files', name),
+                    dict(overwrite=overwrite),
+                    data=src, method='put', auth_header=True)
 
     def mkdir(self, name=None, folder_id='me/skydrive', metadata=dict()):
         """Create a folder with a specified "name" attribute.

setup.py
@@ -14,7 +14,7 @@ except IOError:
 setup(
 
     name='python-onedrive',
-    version='14.06.3',
+    version='14.08.0',
     author='Mike Kazantsev, Antonio Chen',
     author_email='mk.fraggod@gmail.com',
     license='WTFPL',

commit 312b616a42b839f54cf6e330f3632a1ecb2b842f

api.put: fix hangs when uploading files via PUT
Can't seem to make PUT requests (introduced in 9d5f00f70df)
work without forcing "Transfer-Encoding: chunked".

requests seem to strap on correct Content-Length and read/send the
right amount of bytes from source file, yet server apparently waits
for more... or something like that.

onedrive/api_v5.py
@@ -330,6 +330,10 @@ class OneDriveAPIWrapper(OneDriveAuth):
 				return self.put_bits( path_or_tuple,
 					folder_id=folder_id, overwrite=overwrite, downsize=downsize )
 
+		# Force chunked encoding, as uploads hang otherwise
+		# See https://github.com/mk-fg/python-onedrive/issues/30 for details
+		if hasattr(src, 'read'): src = iter(ft.partial(src.read, 200 * 2**10), b'')
+
 		return self( ujoin(folder_id, 'files', name),
 			dict(downsize_photo_uploads=downsize, overwrite=overwrite),
 			data=src, method='put', auth_header=True )

setup.py
@@ -12,7 +12,7 @@ except IOError: readme = ''
 setup(
 
 	name='python-onedrive',
-	version='14.11.14',
+	version='14.11.15',
 	author='Mike Kazantsev, Antonio Chen',
 	author_email='mk.fraggod@gmail.com',
 	license='WTFPL',